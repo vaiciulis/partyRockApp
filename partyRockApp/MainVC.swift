3b04242cc63ad57ff081f859f8cd69d5f612c5f8
Giedrius Vaiciulis <Giedrius@GV-MacBook.local>
2017-05-03 07:21:11 +0100

    UI Design Video end

---
+++ partyRockApp/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  partyRockApp
+//
+//  Created by Giedrius Vaiciulis on 02/05/2017.
+//  Copyright © 2017 Vaiciulis. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   
+    }
+
+
+}
+
